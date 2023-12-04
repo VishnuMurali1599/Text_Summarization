9ed86c41c2c4d43b7a9edccde5c54acccc2659e7
vishnumurali <vishnumurali880@gmail.com>
2023-12-04 11:29:36 +0530

    data ingestion notebook

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.logging import logger
 
-logger.info("welcome to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,6 @@
+# it returns config.yaml angd params.yaml file
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
